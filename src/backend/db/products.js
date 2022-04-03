9ecb2211b3be294320d29d15764602857ab46189
Shubhamkr2610 <shubham1bcabnc18@gmail.com>
2022-04-03 21:39:29 +0530

    Added: data category and added image to product category

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -1,5 +1,5 @@
 import { v4 as uuid } from "uuid";
-import { cartimg1, cartimg2, menShoe, wishlistimg1, trendingShoe1, kidShoe } from "asset";
+import { casual1, casual2, casual3, casual4, formal1, formal2, formal3, formal4, sport1, sport2, sport3, sport4 } from "asset";
 
 /**
  * Product Database can be added here.
@@ -9,111 +9,145 @@ import { cartimg1, cartimg2, menShoe, wishlistimg1, trendingShoe1, kidShoe } fro
 export const products = [
   {
     _id: uuid(),
-    title:"Designed by Bruce Kilgore and introduced in 1982" ,brand:"Nike airforce" , 
+    categoryName: "Sports",
+    title:"Designed by Bruce Kilgore and introduced in 1982" ,
+    brand:"Nike", 
     discountedPrice: 500, 
     actualPrice: 2000,
     discountoff:"(75% off)",
-    srcimg : cartimg1,
+    rating: 2.5,
+    srcimg : sport1,
   },
  
   {
     _id: uuid(),
-    title:"Designed by Bruce Kilgore and introduced in 1982" ,brand:"Nike airforce" , 
+    categoryName: "Sports",
+    title:"Designed by Bruce Kilgore and introduced in 1982" ,
+    brand:"Nike ", 
     discountedPrice: 500, 
     actualPrice: 2000,
     discountoff:"(75% off)",
-    srcimg : cartimg2,
+    rating: 3.5,
+    srcimg : sport2,
   },
  
   {
     _id: uuid(),
-    title:"Designed by Bruce Kilgore and introduced in 1982" ,brand:"Nike airforce" , 
+    categoryName: "Sports",
+    title:"Designed by Bruce Kilgore and introduced in 1982" ,
+    brand:"Nike " , 
     discountedPrice: 500, 
     actualPrice: 2000,
     discountoff:"(75% off)",
-    srcimg : menShoe,
+    rating: 4.5 ,
+    srcimg : sport3,
   },
  
   {
     _id: uuid(),
-    title:"Designed by Bruce Kilgore and introduced in 1982" ,brand:"Nike airforce" , 
+    categoryName: "Sports",
+    title:"Designed by Bruce Kilgore and introduced in 1982" ,
+    brand:"Nike " , 
     discountedPrice: 500, 
     actualPrice: 2000,
     discountoff:"(75% off)",
-    srcimg : wishlistimg1,
+    rating: 3.5,
+    srcimg : sport4,
   },
  
   {
     _id: uuid(),
-    title:"Designed by Bruce Kilgore and introduced in 1982" ,brand:"Nike airforce" , 
+    categoryName: "Casual",
+    title:"Designed by Bruce Kilgore and introduced in 1982" ,
+    brand:"Nike " , 
     discountedPrice: 500, 
     actualPrice: 2000,
     discountoff:"(75% off)",
-    srcimg : trendingShoe1,
+    rating: 4,
+    srcimg : casual1,
   },
  
   {
     _id: uuid(),
-    title:"Designed by Bruce Kilgore and introduced in 1982" ,brand:"Nike airforce" , 
+    categoryName: "Casual",
+    title:"Designed by Bruce Kilgore and introduced in 1982" ,
+    brand:"Nike " , 
     discountedPrice: 500, 
     actualPrice: 2000,
     discountoff:"(75% off)",
-    srcimg : kidShoe,
+    rating: 3,
+    srcimg : casual2,
   },
  
   {
     _id: uuid(),
-    title:"Designed by Bruce Kilgore and introduced in 1982" ,brand:"Nike airforce" , 
+    categoryName: "Casual",
+    title:"Designed by Bruce Kilgore and introduced in 1982" ,
+    brand:"Nike " , 
     discountedPrice: 500, 
     actualPrice: 2000,
     discountoff:"(75% off)",
-    srcimg : cartimg1,
+    rating: 3.5,
+    srcimg : casual3,
   },
  
   {
     _id: uuid(),
-    title:"Designed by Bruce Kilgore and introduced in 1982" ,brand:"Nike airforce" , 
+    categoryName: "Casual",
+    title:"Designed by Bruce Kilgore and introduced in 1982" ,
+    brand:"Nike " , 
     discountedPrice: 500, 
     actualPrice: 2000,
     discountoff:"(75% off)",
-    srcimg : cartimg2,
+    rating: 4.5,
+    srcimg : casual4,
   },
  
   {
     _id: uuid(),
-    title:"Designed by Bruce Kilgore and introduced in 1982" ,brand:"Nike airforce" , 
+    categoryName: "Formal",
+    title:"Designed by Bruce Kilgore and introduced in 1982" ,
+    brand:"Nike " , 
     discountedPrice: 500, 
     actualPrice: 2000,
     discountoff:"(75% off)",
-    srcimg : cartimg1,
+    rating: 3.5,
+    srcimg : formal1,
   },
  
   {
     _id: uuid(),
-    title:"Designed by Bruce Kilgore and introduced in 1982" ,brand:"Nike airforce" , 
+    categoryName: "Formal",
+    title:"Designed by Bruce Kilgore and introduced in 1982" ,
+    brand:"Bata" , 
     discountedPrice: 500, 
     actualPrice: 2000,
     discountoff:"(75% off)",
-    srcimg : menShoe,
+    rating: 2.5,
+    srcimg : formal2,
   },
  
   {
     _id: uuid(),
-    title:"Designed by Bruce Kilgore and introduced in 1982" ,brand:"Nike airforce" , 
+    categoryName: "Formal",
+    title:"Designed by Bruce Kilgore and introduced in 1982" ,
+    brand:"WoodLand" , 
     discountedPrice: 500, 
     actualPrice: 2000,
+    rating: 4.5,
     discountoff:"(75% off)",
-    srcimg : cartimg2,
+    srcimg : formal3,
   },
  
   {
     _id: uuid(),
-    title:"Designed by Bruce Kilgore and introduced in 1982" ,brand:"Nike airforce" , 
+    categoryName: "Formal",
+    title:"Designed by Bruce Kilgore and introduced in 1982" ,
+    brand:"ShreeLeather" , 
     discountedPrice: 500, 
     actualPrice: 2000,
+    rating: 4,
     discountoff:"(75% off)",
-    srcimg : cartimg1,
+    srcimg : formal4,
   },
- 
- 
 ];
